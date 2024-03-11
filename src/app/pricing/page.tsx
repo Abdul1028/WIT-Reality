8afdee68db1c3969f61c7dd0f87a74860ba00ee5
Abdul Shaikh <rasoolas2003@gmail.com>
2024-03-11 11:17:38 +0530

    eslint 2 changes

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,27 +1,12 @@
 "use client";
 import { HoverEffect } from "@/components/ui/card-hover-efffect";
 
-export default function CardHoverEffectDemo() {
+export default function page() {
   return (
     <div className="max-w-5xl mx-auto px-8">
-      <HoverEffect items={projects} />
+      <HoverEffect 
+      
+     />
     </div>
   );
 }
-export const projects = [
-  {
-    title: "Pro",
-    description: "A pro tier with 34000 WIT Cents",
-    link: "https://stripe.com",
-  },
-  {
-    title: "Premium",
-    description: "A premium tier with unlimited cents.",
-    link: "https://netflix.com",
-  },
-  {
-    title: "Premium +",
-    description: "A premium + tier dynamic user support also",
-    link: "https://google.com",
-  },
-];
